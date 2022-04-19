481129975778d2c9ca9e7ba4c1878eaa026a3072
Mwinnome <101268966+Mwinnome@users.noreply.github.com>
2022-04-19 11:36:25 +0000

    Update BookForm.js

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -4,16 +4,16 @@ function BookForm() {
   return (
     <div className='row'>
     <span className='col s2'/>
-    <form className='card-col s8'>
+    <form className='card col s8'>
     <div className='row'>
 
     <div className='input-field col s6'>
-    <input id='book-title' type="text" className='validate'/>
+    <input id='book_title' type="text" className='validate'/>
     <label htmlFor='book_title'>Book Tilte</label>
     </div>
 
     <div className='input-field col s6'>
-    <input id='book-author' type="text" className='validate'/>
+    <input id='book_author' type="text" className='validate'/>
     <label htmlFor='book_author'>Book Author</label>
 
     
@@ -25,12 +25,12 @@ function BookForm() {
     <div className='row'>
 
     <div className='input-field col s6'>
-    <input id='book-category' type="text" className='validate'/>
+    <input id='book_category' type="text" className='validate'/>
     <label htmlFor='book_category'>Book Category</label>
     </div>
 
     <div className='input-field col s6'>
-    <input id='book-isbn' type="text" className='validate'/>
+    <input id='book_isbn' type="text" className='validate'/>
     <label htmlFor='book_isbn'>Book ISBN</label>
 
     
@@ -39,7 +39,7 @@ function BookForm() {
 
     <div className='row'>
     <div className='input-field col s12'>
-    <textarea id='book_summary' className='materialise_textarea'></textarea>
+    <textarea id='book_summary' className='materialise-textarea'></textarea>
     <label htmlFor='book_summary'>Book Summary</label>
     </div>
     </div>
